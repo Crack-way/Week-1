95e6100823fd173dc74e04056fc1ff0c1747c33c
Crack-way <rupeshsunuwar2019@gmail.com>
2023-03-20 10:00:36 +0545

    Changes

---
+++ src/Day2.java
@@ -0,0 +1,30 @@
+public class Day2 {
+
+
+    public static void main(String[] args){
+
+        //Datatypes are types data we can store.
+
+        //Following are the primitive data types which are pre built in most programming language.
+
+        //declaring and assigning int value in int data types
+        int a=5;
+
+        //declaring and assigning float value in float data types
+        float b=6.0f;
+
+        //declaring and assigning char in char data types
+        char c='C';
+
+        //declaring and assigning Booleans which is either 0 or 1.
+        Boolean d=true;
+
+
+
+
+
+
+
+
+    }
+}
